606b3e74a26a460ecb444d6f45ec44fa29edf1fa
craigbranscom <craigbranscom@gmail.com>
2019-12-29 11:27:07 -0500

    script updates

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,30 @@
 # contract
 if [[ "$1" == "amend" ]]; then
     contract=amend
-    account=amendtester1
 else
     echo "need contract"
     exit 0
 fi
 
-#network
+# account
+account=$2
+
+# network
 if [[ "$2" == "mainnet" ]]; then 
     url=http://api.tlos.goodblock.io # Telos Mainnet
+    network=Mainnet
 elif [[ "$2" == "testnet" ]]; then
     url=https://testnet.telosusa.io/ # Basho Testnet
+    network=Testnet
 elif [[ "$2" == "local" ]]; then
     url=http://127.0.0.1:8888
+    network=Local
 else
     echo "need network"
     exit 0
 fi
 
-echo ">>> Deploying $contract contract to $account on $2..."
+echo ">>> Deploying $contract contract to $account on Telos $network..."
 
 # eosio v1.8.0
 cleos -u $url set contract $account ./build/$contract/ $contract.wasm $contract.abi -p $account
